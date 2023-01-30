ea48c4b072c96805d5560cb1631263ea821e03a0
Abdullah <AbAlomari95@gmail.com>
2023-01-30 16:05:38 +0300

    Upload cv lab

---
+++ cv.js
@@ -0,0 +1,59 @@
+const cont = document.getElementById("container")
+const element = <>
+    <div id="All">
+    <div id="inside">
+        <div id="sides">
+      <div id="name">
+        <h2>Abdullah Alomari</h2>
+        <p id="job">Information system</p>
+      </div>
+      <div class="cardcont">
+        <div class="mycards">
+          <div class="img-section img-one"></div>
+          <div class="contents">
+            <h2>Contact</h2>
+            <p>
+              Pno: +966 0537357590
+            </p>
+            <p>Email: AbAlomari95@gmail.com</p>
+            <p>Location: Jeddah</p>
+          </div>
+        </div>
+        <div class="mycards">
+          <div class="img-section img-two"></div>
+          <div class="contents">
+            <h2>Experience</h2>
+            <p>Networking</p>
+            <p>IT Infrastructure</p>
+            <p>Python</p>
+          </div>
+        </div>
+        <div class="mycards">
+          <div class="img-section img-three"></div>
+          <div class="contents">
+            <h2>Education</h2>
+            <h4>
+              King Abdulaziz university</h4>
+              <p id="subject">Computin and IT</p>
+          </div>
+        </div>
+        <div class="mycards">
+          <div class="img-section img-two"></div>
+          <div class="contents">
+            <h2>Certificate</h2>
+            <p>Intensive 30 English in LSI, Campridge UK</p>
+            <br/>
+            <p>IELTS: SCORE 6</p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    
+
+
+
+    </>
+ReactDOM.render(element, cont);
